1bc92ea5f0fbe79fb635474f59a67ed8c4fdf28e
DESKTOP-BEAR877\sfsfk <sfsfkj@gmail.com>
2020-05-07 11:59:24 +0900

    import function that use tf in module

--- imgFetch.py
+++ imgFetch.py
@@ -1,6 +1,10 @@
 import glob
 import json
 import numpy as np
+from PIL import Image
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 def make_list():
     f = open('image/fileStructure.json', mode='r', encoding='utf-8')
@@ -35,3 +39,33 @@ def get_path_label(category):
     label_list = [make_onehot(name_list, path).tolist() for path in img_list]
 
     return img_list, label_list
+
+def showList(img_list, label_list):
+    print("\n**************************")
+    print("image path list length : ",len(img_list))
+    print("label list length : ",len(label_list))
+    print("test case 0")
+    print(img_list[0])
+    print(label_list[0])
+    print(" ")
+
+def _read_py_function(path, label):
+    img = np.array(Image.open(path))
+    label = np.array(label, dtype=np.float32)
+    return img.astype(np.float32), label
+
+
+def get_iterator_next_element(category, batch_size):
+    img_list, label_list = get_path_label(category)
+
+    dataset = tf.data.Dataset.from_tensor_slices((img_list, label_list))
+    dataset = dataset.map(lambda img_list, label_list: tuple(tf.py_func(_read_py_function,[img_list, label_list], [tf.float32, tf.float32])))
+
+    dataset = dataset.repeat()
+    dataset = dataset.shuffle(buffer_size=(int(len(img_list) *0.4) + 3 * batch_size))
+    dataset = dataset.batch(batch_size)
+
+    iterator = dataset.make_initializable_iterator()
+    next_element = iterator.get_next()
+    print("batch is ready")
+    return iterator, next_element
